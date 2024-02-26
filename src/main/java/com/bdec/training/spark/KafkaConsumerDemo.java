e59393bb3c4c2bd9a1a6a052c185c19c8c407b55
donotech <36134071+donotech@users.noreply.github.com>
2024-02-26 12:11:03 +0530

    added kafka consumer

---
+++ src/main/java/com/bdec/training/spark/KafkaConsumerDemo.java
@@ -0,0 +1,46 @@
+package com.bdec.training.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+
+import java.util.concurrent.TimeoutException;
+
+public class KafkaConsumerDemo {
+    public static void main(String[] args) {
+        String winUtilPath = "C:\\softwares\\winutils\\";
+        if(System.getProperty("os.name").toLowerCase().contains("win")) {
+            System.out.println("detected windows");
+            System.setProperty("hadoop.home.dir", winUtilPath);
+            System.setProperty("HADOOP_HOME", winUtilPath);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("SocgenJava")
+                .master("local[*]")
+                .getOrCreate();
+
+        spark.conf().set("spark.sql.shuffle.partitions", "2");
+        spark.sparkContext().setLogLevel("WARN");
+
+        Dataset<Row> df = spark.readStream().format("kafka")
+                .option("kafka.bootstrap.servers", "localhost:9092")
+                .option("startingOffsets", "earliest")
+                .option("subscribe", "socgenl2").load();
+
+        StreamingQuery query;
+        try {
+            query = df.withColumn("value_string",
+                            df.col("value").cast("string"))
+                    .writeStream().outputMode("append")
+                    .format("console").start();
+            query.awaitTermination();
+        } catch (TimeoutException | StreamingQueryException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
